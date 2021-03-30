2d6fdee52aedef1ad4ccd1dac2e2978e91436c46
Thomas Le Vaou <thomas.levaou@protonmail.com>
2021-03-30 21:38:53 +0200

    End chapter 6

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -23,9 +24,20 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
+/*        return new Response(sprintf(
             'Future page to show one space article: %s',
             $slug
-        ));
+        ));*/
+
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', [
+           'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments
+        ]);
     }
 }
